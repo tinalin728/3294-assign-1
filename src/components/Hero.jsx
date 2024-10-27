c70b2cb95b6909996931f886fb0618665c7d3bae
tinalin728 <yuting.lin728@gmail.com>
2024-10-27 11:47:59 -0700

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,49 +1,74 @@
-import React, { useLayoutEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import productsData from '../data/products.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleLeft } from '@fortawesome/free-solid-svg-icons'
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
-
+import { gsap } from 'gsap';
 
 function Hero() {
     //fetching product with index for hero banners
     const heroBanner1 = productsData.hero[0]
     const heroBanner2 = productsData.hero[1]
 
-    const hero1Ref = useRef(null);
-    const hero2Ref = useRef(null);
+    // set mobile screen size detection (to darken first banner img when its in mobile view)
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
 
-    useLayoutEffect(() => {
+    //creates a ref to directly access the DOM element of the first banner (to animate)
+    const firstHeroRef = useRef();
 
-        const ctx = gsap.context(() => {
+    useEffect(() => {
+        const handleScroll = () => {
+            const stickyElement = firstHeroRef.current;
+            const stickyTop = stickyElement.getBoundingClientRect().top;
+            const scrollY = window.scrollY;
+            const maxScroll = 1500;
 
-            gsap.to(hero1Ref.current, {
-                y: '-200%', // Move up out of view
-                scrollTrigger: {
-                    trigger: hero1Ref.current,
-                    start: "top top", // When Hero 1's top hits the top of the viewport
-                    end: "bottom top",
-                    scrub: 1,
-                },
-            });
+            let brightnessValue = 1 - Math.min(scrollY / maxScroll, 1);
 
-        });
+            if (stickyTop <= 0) {
+                gsap.to(stickyElement, {
+                    duration: 0.8,
+                    filter: `brightness(${brightnessValue})`,
+                });
+            } else {
+                gsap.to(stickyElement, {
+                    duration: 0.8,
+                    filter: 'brightness(1)',
+                });
+            }
+        };
 
-        return () => ctx.revert();
-    }, [])
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+            window.removeEventListener('resize', handleResize);
+
+        };
+    }, []);
 
 
     return (
         <>
-            <section className='mt-[80px] md:mt-[200px] relative'>
-                <div ref={hero1Ref} className=' first-hero absolute top-0 left-0 bg-gradient-to-br from-white to-soft-blue min-h-[800px] w-full z-20'>
-                    <div className='relative container mx-auto flex flex-col items-center justify-center lg:flex-row'>
+            <section className='pt-[80px] md:pt-[200px] h-full relative'>
+                <div ref={firstHeroRef} className="wrapper first-hero sticky top-0 bottom-0 min-h-[80vh]" style={{
+                    backgroundImage: isMobile
+                        ? `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${heroBanner1.src})`
+                        : `url(${heroBanner1.src})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'bottom',
+                    backgroundRepeat: 'no-repeat',
+                }} >
+
+                    <div className='relative container mx-auto min-h-[80vh] flex flex-col items-center justify-center lg:flex-row'>
                         <div className='flex-1  text-center mt-10 lg:text-left'>
-                            <h1 className='mb-4'>Discover Your New Adorable Monster Friend!
+                            <h1 className='mb-4 text-white'>Discover Our New Friendly Friends!
                             </h1>
-                            <p className='mb-4'>
+                            <p className='mb-8 text-gray-200'>
                                 Unleash your imagination with our unique collection of soft, lovable monsters designed to bring joy and creativity to your life. Each monster is crafted with care and is ready to become your next cuddly companion!
                             </p>
                             <div className='relative inline-flex items-center group'>
@@ -52,12 +77,8 @@ function Hero() {
                             </div>
                         </div>
 
-                        <div className='flex-1'>
-                            <img src={heroBanner1.src} alt={heroBanner1.name} className=""
-                            />
-                        </div>
 
-                        <div className='hidden md:block'>
+                        <div className='hidden md:flex-1 md:block'>
                             <div className='flex items-center gap-4 -rotate-90 absolute bottom-1/2 right-0'>
                                 <FontAwesomeIcon icon={faAngleLeft} className='text-xl text-gray-600 animate-upDown' />
                                 <span className='uppercase tracking-wide text-lg text-gray-600'>Scroll</span>
@@ -66,31 +87,33 @@ function Hero() {
                     </div>
                 </div>
 
-                <div ref={hero2Ref} className='second-hero z-10 relative'
-                    style={{
-                        backgroundImage: `url(${heroBanner2.src})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        backgroundRepeat: 'no-repeat',
-                    }} >
-                    <div className='container mx-auto w-full min-h-[800px] flex items-center'>
-                        <div className='text-center mt-10 lg:text-left'>
-                            <h1 className='mb-4 text-white'>Enter the Haunted Hideaway!
-                            </h1>
 
-                            <p className='mb-4 text-white'>
-                                Packed with potions, pumpkins, and paranormal thrills!
-                            </p>
+                <div className='wrapper second-hero relative'>
+                    <div className='z-10 relative'
+                        style={{
+                            backgroundImage: `url(${heroBanner2.src})`,
+                            backgroundSize: 'cover',
+                            backgroundPosition: 'center',
+                            backgroundRepeat: 'no-repeat',
+                        }} >
+                        <div className='container mx-auto w-full min-h-[80vh] flex items-center'>
+                            <div className='text-center mt-10 lg:text-left'>
+                                <h1 className='mb-4 text-white'>Enter the Haunted Hideaway!
+                                </h1>
 
-                            <div className='relative inline-flex items-center group'>
-                                <a href="/" className='bg-black border-white text-white border-2 px-6 py-2 inline-flex z-10 cursor-pointer group'> Shop Now</a>
-                                <div className='absolute w-full h-full border-2 border-white top-2 left-2 z-0 transition-all duration-500 ease-in-out group-hover:top-0 group-hover:left-0'></div>
+                                <p className='mb-8 text-white'>
+                                    Packed with potions, pumpkins, and paranormal thrills!
+                                </p>
+
+                                <div className='relative inline-flex items-center group'>
+                                    <a href="/" className='bg-black border-white text-white border-2 px-6 py-2 inline-flex z-10 cursor-pointer group'> Shop Now</a>
+                                    <div className='absolute w-full h-full border-2 border-white top-2 left-2 z-0 transition-all duration-500 ease-in-out group-hover:top-0 group-hover:left-0'></div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-            </section>
+            </section >
         </>
     )
 }
